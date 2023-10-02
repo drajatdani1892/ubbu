ffa062cd5781b8de1cf7b20bd29e2b4e4386767e
MuhammadDrajat <drajatdani1892@gmail.com>
2023-10-02 14:27:24 +0700

    Chore: Add Section Kegiatan

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,60 @@ function App() {
         </div>
       </section>
       {/* End of Hero */}
+
+      {/* Section Kegiatan */}
+      <section className="bg-slate-900 py-12">
+        <div className="container mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl font-semibold mb-2">Kegiatan UBBU</h2>
+            <p className="text-gray-400">
+              Berikut adalah kegiatan rutin yang dilaksanakan oleh UKM UBBU
+            </p>
+          </div>
+          <div className="flex flex-col mt-8 md:flex-row md:justify-center gap-4">
+            {/* Card */}
+            <div className="card-features bg-slate-800 p-4 rounded-lg shadow-md text-center max-w-xs mx-auto md:mx-0">
+              <img
+                src="/1.jpg"
+                alt="Image Features"
+                className="h-32 w-32 mx-auto rounded-full"
+              />
+              <h3 className="text-xl font-semibold mt-4">3x3 Competitions</h3>
+              <p className="text-gray-400 mt-2">
+                Experience thrilling 3x3 basketball competitions with us.
+              </p>
+            </div>
+            {/* End of Card */}
+            {/* Card */}
+            <div className="card-features bg-slate-800 p-4 rounded-lg shadow-md text-center max-w-xs mx-auto md:mx-0">
+              <img
+                src="/2.jpg"
+                alt="Image Features"
+                className="h-32 w-32 mx-auto rounded-full"
+              />
+              <h3 className="text-xl font-semibold mt-4">3x3 Competitions</h3>
+              <p className="text-gray-400 mt-2">
+                Experience thrilling 3x3 basketball competitions with us.
+              </p>
+            </div>
+            {/* End of Card */}
+            {/* Card */}
+            <div className="card-features bg-slate-800 p-4 rounded-lg shadow-md text-center max-w-xs mx-auto md:mx-0">
+              <img
+                src="/3.jpg"
+                alt="Image Features"
+                className="h-32 w-32 mx-auto rounded-full"
+              />
+              <h3 className="text-xl font-semibold mt-4">3x3 Competitions</h3>
+              <p className="text-gray-400 mt-2">
+                Experience thrilling 3x3 basketball competitions with us.
+              </p>
+            </div>
+            {/* End of Card */}
+          </div>
+        </div>
+      </section>
+      {/* End Of Kegiatan */}
     </div>
   );
 }
